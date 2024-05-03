7fd1f0cfeb04a3ef8d794234012271215290fcac
Rayyan-P <159940781+Rayyan-P@users.noreply.github.com>
2024-05-03 02:45:46 +0000

    New Version

--- Unit-4-Lab-2-Weather-App/city.py
+++ Unit-4-Lab-2-Weather-App/city.py
@@ -1,7 +1,77 @@
 class City:
 	"""Stores weather information for a city"""
-	def __init__(self):
-		pass
-		
+	def __init__(self, name, temp_in_K, desc, feel_K, min_temp_K, max_temp_K, wind):
+		self.name = name
+		self.temp = temp_in_K
+		self.desc = desc
+		self.feel = feel_K
+		self.min = min_temp_K
+		self.max = max_temp_K
+		self.wind = wind
+		self.tempC = "None"
+		self.tempF = "None"
+		self.feelC = "None"
+		self.feelF = "None"
+		self.maxC = "None"
+		self.maxF = "None"
+		self.minC = "None"
+		self.minF = "None"
+
 	def __str__(self):
-		pass
+		return f"City - {self.name}: \n\t Temperature (C): {self.tempC} \n\t Temperature (F): {self.tempF} \n\t Feels like (C): {self.feelC} \n\t Feels like (F): {self.feelF} \n\t Minimum Temperature (C): {self.minC} \n\t Minimum Temperature (F): {self.minF} \n\t Maximum Temperature (C): {self.maxC} \n\t Maximum Temperature (F): {self.maxF} \n\t Weather: {self.desc} \n\t Wind Speed (MPH): {self.wind}"
+	
+	def get_name(self):
+		return self.name
+	
+	def get_desc(self):
+		return self.desc
+	
+	def get_temp_in_K(self):
+		return self.temp
+
+	def get_temp_in_C(self):
+		cTemp = self.temp - 273.15
+		self.tempC = cTemp
+		return self.tempC
+
+	def get_temp_in_F(self):
+		fTemp = ((self.temp - 273.15) * 1.8) + 32
+		self.tempF = fTemp
+		return self.tempF 
+	
+	def get_feel_in_C(self):
+		cFeel = self.feel - 273.15
+		self.feelC = cFeel
+		return self.feelC
+
+	def get_feel_in_F(self):
+		fFeel = ((self.feel - 273.15) * 1.8) + 32
+		self.feelF = fFeel
+		return self.feelF
+
+	def get_min_in_C(self):
+		cMin = self.min - 273.15
+		self.minC = cMin
+		return self.minC
+
+	def get_min_in_F(self):
+		fMin = ((self.min - 273.15) * 1.8) + 32
+		self.minF = fMin
+		return self.minF 
+	
+	def get_max_in_C(self):
+		cMax = self.max - 273.15
+		self.maxC = cMax
+		return self.maxC
+
+	def get_max_in_F(self):
+		fMax = ((self.max - 273.15) * 1.8) + 32
+		self.maxF = fMax
+		return self.maxF
+	
+	def get_wind_speed(self):
+		speedWind = self.wind * 2.237
+		self.wind = speedWind
+		return self.wind
+	
+	
--- Unit-4-Lab-2-Weather-App/flask_app.py
+++ Unit-4-Lab-2-Weather-App/flask_app.py
@@ -11,7 +11,7 @@ from city import City
 
 # list of City objects
 cities = []
-API_KEY = None
+API_KEY = "dcb9deb505067260a9d290e0f4030b13"
 
 def get_data(city):
 	"""
@@ -23,17 +23,48 @@ def get_data(city):
 	Returns:
 	list: A list of data fetched from the API, with the city name as the first element.
 	"""
+
 	# list of data fetched from API
 	data = [city.title()]
 	# insert code to get API data (Step 3)
+
+	# initialize your API key here
+	API_KEY = 'dcb9deb505067260a9d290e0f4030b13'
+
+	# request data from API and retrieve json data response
+	url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&APPID={API_KEY}'
+	response = requests.get(url)
+
+	# convert json response into python dictionary
+	response = response.json()
+	print(response)
+
+	dict = response.get('main')
+	current_temp = dict.get('temp')
+	data.append(current_temp)
+
+	# get current description from dictionary and add it to the data list
+	current_desc = response.get('weather')[0].get('description')
+	data.append(current_desc.title())
+
+	temp_feel = response.get("main").get("feels_like")
+	data.append(temp_feel)
+
+	min_temp = response.get("main").get("temp_min")
+	data.append(min_temp)
+
+	max_temp = response.get("main").get("temp_max")
+	data.append(max_temp)
+
+	windSpeed = response.get("wind").get("speed")
+	data.append(windSpeed)
+
 	return data
 
 app = Flask(__name__)
 
 @app.route('/', methods = ['GET'])
 def index():
-	return f"""<h1>Hello, World!</h1> 
- 	<p>The date/time is {datetime.now()}.</p>
- 	"""
+	return render_template('index.html') 
 
 app.run(host='0.0.0.0', port=8080) # any code below 'app.run' line won't run
