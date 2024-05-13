6198a175eead36f45a44729e2368b2956813bb8b
Rayyan-P <159940781+Rayyan-P@users.noreply.github.com>
2024-05-13 03:14:37 +0000

    .

--- Unit-4-Lab-2-Weather-App/flask_app.py
+++ Unit-4-Lab-2-Weather-App/flask_app.py
@@ -106,14 +106,132 @@ def get_pollution():
 	lat = 41.748489
 	lon = 88.186111
 	dataPol = []
+	indexCalc = 0
+	coIndex = 0
+	no2Index = 0
+	o3Index = 0
+	so2Index = 0
+	pm2_5Index = 0
+	pm10Index = 0
 
 	urlExp3 = f"http://api.openweathermap.org/data/2.5/air_pollution?lat={lat}&lon={lon}&appid={API_KEY}"
 	responseExp3 = requests.get(urlExp3)
 	responseExp3 = responseExp3.json()
 	print(responseExp3)
 
-	co = responseExp3.get("list").get("components").get("co")
+	coinit = responseExp3.get("list")
+	co = coinit[0].get("components").get("co")
+	no2init = responseExp3.get("list")
+	no2 = no2init[0].get("components").get("no2")
+	o3init = responseExp3.get("list")
+	o3 = o3init[0].get("components").get("o3")
+	so2init = responseExp3.get("list")
+	so2 = so2init[0].get("components").get("so2")
+	pm2_5init = responseExp3.get("list")
+	pm2_5 = pm2_5init[0].get("components").get("pm2_5")
+	pm10init = responseExp3.get("list")
+	pm10 = pm10init[0].get("components").get("pm10")
 
+	if co <= 4400:
+		indexCalc += 1
+		coIndex = 1
+	elif co >= 4400 and co <= 9400:
+		indexCalc += 2
+		coIndex = 2
+	elif co >= 9400 and co <= 12400:
+		indexCalc += 3
+		coIndex = 3
+	elif co >= 12400 and co <= 15400:
+		indexCalc += 4
+		coIndex = 4
+	elif co > 15400:
+		indexCalc += 5
+		coIndex = 5
+
+	if no2 <= 40:
+		indexCalc += 1
+		no2Index = 1
+	elif no2 >= 40 and no2 <= 70:
+		indexCalc += 2
+		no2Index = 2
+	elif no2 >= 70 and no2 <= 150:
+		indexCalc += 3
+		no2Index = 3
+	elif no2 >= 150 and no2 <= 200:
+		indexCalc += 4
+		no2Index = 4
+	elif no2 > 200:
+		indexCalc += 5
+		no2Index = 5
+
+	if o3 <= 40:
+		indexCalc += 1
+		o3Index = 1
+	elif o3 >= 40 and o3 <= 70:
+		indexCalc += 2
+		o3Index = 2
+	elif o3 >= 70 and o3 <= 150:
+		indexCalc += 3
+		o3Index = 3
+	elif o3 >= 150 and o3 <= 200:
+		indexCalc += 4
+		o3Index = 4
+	elif o3 > 200:
+		indexCalc += 5
+		o3Index = 5
+
+	if so2 <= 20:
+		indexCalc += 1
+		so2Index = 1
+	elif so2 >= 20 and so2 <= 80:
+		indexCalc += 2
+		so2Index = 2
+	elif so2 >= 80 and so2 <= 250:
+		indexCalc += 3
+		so2Index = 3
+	elif so2 >= 250 and so2 <= 350:
+		indexCalc += 4
+		so2Index = 4
+	elif so2 > 350:
+		indexCalc += 5
+		so2Index = 5
+
+	if pm2_5 <= 10:
+		indexCalc += 1
+		pm2_5Index = 1
+	elif pm2_5 >= 10 and pm2_5 <= 25:
+		indexCalc += 2
+		pm2_5Index = 2
+	elif pm2_5 >= 25 and pm2_5 <= 50:
+		indexCalc += 3
+		pm2_5Index = 3
+	elif pm2_5 >= 50 and pm2_5 <= 75:
+		indexCalc += 4
+		pm2_5Index = 4
+	elif pm2_5 > 75:
+		indexCalc += 5
+		pm2_5Index = 5
+
+	if pm10 <= 20:
+		indexCalc += 1
+		pm10Index = 1
+	elif pm10 >= 20 and pm10 <= 50:
+		indexCalc += 2
+		pm10Index = 2
+	elif pm10 >= 50 and pm10 <= 100:
+		indexCalc += 3
+		pm10Index = 3
+	elif pm10 >= 100 and pm10 <= 200:
+		indexCalc += 4
+		pm10Index = 4
+	elif pm10 > 200:
+		indexCalc += 5
+		pm10Index = 5
+
+	indexCalc = round(indexCalc / 6, 2)
+	percentage = round(indexCalc / 5, 2)
+
+	return [indexCalc, coIndex, no2Index, o3Index, so2Index, pm2_5Index, pm10Index, percentage]
 
 
 
@@ -172,6 +290,8 @@ def page2():
 @app.route('/pollution.html', methods = ['GET'])
 def page3():
 	"""Page to off the webapp"""
-	return render_template("pollution.html")
+	cityPol = get_pollution()
+
+	return render_template("pollution.html", polIndex = cityPol[0], coInd = cityPol[1], no2Ind = cityPol[2], o3Ind = cityPol[3], so2Ind = cityPol[4], pm2_5Ind = cityPol[5], pm10Ind = cityPol[6], percent = cityPol[7])
 
 app.run(host='0.0.0.0', port=8080) # any code below 'app.run' line won't run
